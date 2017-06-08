f064533bbceaf53e99bd1c532ed51f2a70865557
Harvey Connor <harvey@zoonix.tv>
2017-06-08 10:45:23 +1000

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,14 @@ Route::get('/about', function () {
 
 Route::get('/gallery/graphics', 'GalleryController@showGraphics')->name('gallery.graphics');
 //Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //Route::get('/home', 'HomeController@index');
 
 Route::group(['middleware' => 'auth'],function(){
